1046d2f1a4ab7a2c3fe2263b3dd4e13a17187bc0
Stephane Bellity <sbellity@gmail.com>
2013-01-07 23:49:17 +0100

    adding route-recognizer to deps

--- app/app.js
+++ app/app.js
@@ -1,14 +1,13 @@
 define(['lib/hull'], function(Hull) {
 
-  Hull({
+  window.app = Hull({
     name: "super app",
     orgUrl: 'http://aura.hull.dev',
     appId: '50e842b8cf9f4f1f00000004',
     debug: true,
     widgets: {
-      sources: { 'default' : '/widgets', 'example' : 'http://example.aura-express.dev/widgets' }
+      sources: { 'default' : 'widgets/' }
     }
   });
-
 
 });
