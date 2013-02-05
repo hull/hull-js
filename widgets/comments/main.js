323804782b15ae8d1be63d816e14d1957391c916
Stephane Bellity <sbellity@gmail.com>
2013-02-05 21:27:49 +0100

    comments widget : fix double submit

--- widgets/comments/main.js
+++ widgets/comments/main.js
@@ -26,14 +26,17 @@ define({
   debug: true,
 
   initialize: function () {
-    this.sandbox.on("collection.hull." + this.id + ".comments.**", function() { this.refresh(); }.bind(this));
+    this.sandbox.on("collection.hull." + this.id + ".comments.**", function() {
+      console.warn("Calling refresh !", arguments);
+      this.refresh();
+    }.bind(this));
   },
 
   //@FIX Cache is broken for datasources declared as objects
   datasources: {
     comments: ":id/comments"
   },
-  
+
   actions: {
     comment: function (elt, evt, data) {
       var description = this.$el.find("textarea").val();
@@ -41,8 +44,6 @@ define({
         var comment = this.datasources.comments.create({
           description: description
         });
-        var xhr = comment.save();
-        xhr.promise().then(_.bind(this.render, this, null, null));
       }
       evt.stopPropagation();
     }
