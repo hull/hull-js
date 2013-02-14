0d1f7474511054cfed782328035ed9f1d09578e4
Victor Coulon <victor@hull.io>
2013-02-14 15:00:40 +0100

    fix registration validation

--- widgets/registration/default_fields.js
+++ widgets/registration/default_fields.js
@@ -1,3 +1,5 @@
+// Need help for patterns ?
+// ==> http://html5pattern.com/
 define(function() { return [
   {
     name : "name",
@@ -5,24 +7,62 @@ define(function() { return [
     label : "Nom",
     value : "",
     placeholder : "Bob",
-    error : "Please enter your name"
+    error : "Please enter your name",
+    required: true,
+    autocomplete: "off"
+  },
+  {
+    name : "color",
+    type : "text",
+    label : "Your favorite color",
+    value : "",
+    placeholder : "#fff",
+    error : "Please enter a valid color",
+    required: true,
+    pattern: "^#?([a-fA-F0-9]{6}|[a-fA-F0-9]{3})$"
+  },
+  {
+    name : "url",
+    type : "url",
+    label : "Url",
+    value : "",
+    required: true,
+    placeholder : "http://www.google.fr",
+    error : "Invalid URL"
   },
   {
     name : "email",
-    type : "text",
+    type : "email",
     label : "Email",
     value : "",
-    placeholder : "bob@host.com",
+    required: true,
+    placeholder : "you@awesome.com",
     error : "Invalid Email"
+  },
+  {
+    name : "tel",
+    type : "tel",
+    label : "Tel",
+    value : "",
+    required: true,
+    placeholder : "123-456-7890",
+    error : "Invalid Tel"
   },
   {
     name : "gender",
     type : "select",
     label : "Gender",
+    required: true,
     options: [
       {"label": "Male", "value":"Male"},
       {"label": "Female", "value":"Female"}
     ],
     error : "Please select something"
+  },
+  {
+    name : "notifications",
+    type : "checkbox",
+    label : "",
+    checkboxLabel: "Subscribe to notifications"
   }
-]});
+];});
