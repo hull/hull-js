183bc47cffd610ae551e59e835fa27f731225f18
Romain Dardour <romain@hull.io>
2013-02-25 12:36:08 +0100

    Fix dox widgets, adapt markup to not crap out, update Prism.js

--- widgets/dox_list/main.js
+++ widgets/dox_list/main.js
@@ -2,12 +2,12 @@ define({
   type: "Hull",
   templates: ["dox_list"],
   options: {
-    source:'http://hull-js.s3.amazonaws.com/0.2.0/docs'
+    source:'http://hull-js.s3.amazonaws.com/0.2.0/docs/'
   },
 
   datasources: {
     widgets: function() {
-      return $.getJSON(this.options.source+'/index.json');
+      return $.getJSON(this.options.source+'index.json');
     }
   },
   beforeRender: function(data) {
