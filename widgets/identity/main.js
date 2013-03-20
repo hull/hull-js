42a5183fbfde51330d8d8468a48f4e49d0ea4c4a
Jimmy Oliger <hi@jimmyoliger.com>
2013-03-20 17:22:42 +0100

    Clean up Identity widget.
    - Documentation
    - Single quote for all the string.

--- widgets/identity/main.js
+++ widgets/identity/main.js
@@ -1,42 +1,32 @@
 /**
- * # Widget `identity`
+ * # Identity
  *
- * This widget is identified as `identity@hull`  
- * Use it to indicate whether the user is logged in or not, and display login buttons if not.
+ * Allow users to log in with auth services that you have hooked on your app.
  *
+ * ## Template
+ *
+ * - `identity`: Show loggin buttons if the user isn't logged, display his name
+ *   if he is.
  */
-
-
-
 define({
-  type: "Hull",
-  
-  /**
-   * @name Templates
-   * #### `identity/identity`
-   * 
-   * Displays login buttons or the name of the user, depending on its logged in state
-   *
-   */
-  templates: ['identity'],
+  type: 'Hull',
 
-  // Which events to refresh the widget on.
+  templates: [
+    'identity'
+  ],
+
   refreshEvents: ['model.hull.me.change'],
 
   initialize: function() {
-    this.authServices = _.map(this.sandbox.config.services.types.auth, function(s) { return s.replace(/_app$/, ''); })
+    this.authServices = _.map(this.sandbox.config.services.types.auth, function(s) {
+      return s.replace(/_app$/, '');
+    });
+
     if (_.isEmpty(this.authServices)) {
-      console.error("No Auth services configured. please add one to be able to authenticate users.");
+      console.error('No Auth services configured. please add one to be able to authenticate users.');
     }
   },
 
-  /**
-   * Process the resolved datasources before sending them to the template
-   *
-   * @param {Object} data The resolved requests from the `datasources` object
-   * @return {Object|Promise|undefined}     The data after you processed it.
-   * @name beforeRender
-   */
   beforeRender: function(data) {
     data.authServices = this.authServices || [];
     return data;
