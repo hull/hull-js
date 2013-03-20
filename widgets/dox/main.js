db05935fbd191bb4b36bbaad5dae385b52fdda55
Jimmy Oliger <hi@jimmyoliger.com>
2013-03-20 18:17:04 +0100

    Ignore /*! */ comments

--- widgets/dox/main.js
+++ widgets/dox/main.js
@@ -1,19 +1,29 @@
 define(['./prism'], function(){
   return {
     type: "Hull",
-    templates: ["dox", "widget","template"],
+
+    templates: [
+      "dox",
+      "widget",
+      "template"
+    ],
+
     datasources: {
       dox: function() {
         var widgets=this.options.inspect.split(',');
-        var source = this.options.source || 'http://hull-js.s3.amazonaws.com/' + Hull.version + '/docs/'
+        var source = this.options.source || 'http://hull-js.s3.amazonaws.com/' + Hull.version + '/docs/';
+
         var promises = _.map(widgets,function(w){
           var s = w.replace(/^\s+|\s+$/g, ''); //IE-compatible .trim()
           var dfd = $.getJSON(source+s+'/main.json');
           return dfd;
         });
+
+        console.log("DOX");
         return $.when.apply(this, promises);
       }
     },
+
     beforeRender: function(data) {
       data.dox = data.dox[0];
       data.inspect = this.options.inspect;
@@ -29,8 +39,9 @@ define(['./prism'], function(){
       });
       return data;
     },
+
     afterRender: function(){
-      Prism.highlightAll()
+      Prism.highlightAll();
     }
-  }
+  };
 });
