02458ca1e68010dc2a34c7a4ec7677ac7540ba88
Xavier Cambar <xcambar@gmail.com>
2013-04-22 14:42:27 +0200

    Wraps there tests in the correct describe
    encapsulates `before` hooks

--- spec/lib/hull_spec.js
+++ spec/lib/hull_spec.js
@@ -1,78 +1,81 @@
-/*global $:true,sinon:true, define:true, describe:true, it:true, before:true, after:true, beforeEach:true */
+/*global sinon:true, define:true, describe:true, it:true, after:true, beforeEach:true */
 define(['../support/mocks/app'], function (appMock) {
   "use strict";
 
-  // Mocking dependencies of  lib/hull
-  beforeEach(appMock.createApp);
+  describe('hull main module', function () {
+    // Mocking dependencies of  lib/hull
+    beforeEach(appMock.createApp);
 
-  after(function () {
-    appMock.resetApp();
-    require.undef('lib/hull');
-    require.undef('aura/aura');
-    require.undef('lib/hullbase');
-  });
-
-  describe("Booting the application", function () {
-    it("should return a function", function () {
-      appMock.hullInit.should.be.a('Function');
-    });
-  });
-
-  describe("Evaluating the module", function () {
-    beforeEach(appMock.resetApp);
-
-    it("should return an object with 'app' and 'config key'", function () {
-      var conf = {};
-      appMock.hullInit(conf).should.contain.keys('config');
-      appMock.hullInit(conf).should.contain.keys('app');
-    });
-    it("should have an Aura application as the value of the 'app' config", function () {
-      var conf = {};
-      appMock.hullInit(conf).app.should.eql(appMock.app);
-    });
-    it("should return the config passed to the function into the 'config' property", function () {
-      var conf = {
-        foo: "FOO",
-        bar: "BAR",
-        baz: "BAZ"
-      };
-      appMock.hullInit(conf).config.should.contain.keys(Object.keys(conf));
-    });
-    it("should add a 'namespace property in the onfig and set it to 'Hull'", function () {
-      var conf = {};
-      appMock.hullInit(conf).config.should.contain.keys('namespace');
-      appMock.hullInit(conf).config.namespace.should.eql('hull');
-    });
-  });
-
-  describe("should give feedback", function () {
-    beforeEach(appMock.resetApp);
-
-    it("should throw an exception if the init fails and no errback is provided", function (done) {
-      appMock.initDeferred.always(function () { done(); });
-      try {
-        appMock.hullInit({});
-      } catch (e) {
-        done();
-      }
-      appMock.initDeferred.reject();
+    after(function () {
+      appMock.resetApp();
+      require.undef('lib/hull');
+      require.undef('aura/aura');
+      require.undef('lib/hullbase');
     });
 
-    it("should execute the errback in case of error", function () {
-      appMock.app.start.returns(appMock.initDeferred);
-      var errb = sinon.spy();
-      appMock.hullInit({}, null, errb);
-      appMock.initDeferred.reject();
-      errb.should.have.been.called;
+    describe("Booting the application", function () {
+      it("should return a function", function () {
+        appMock.hullInit.should.be.a('Function');
+      });
     });
 
-    it("should execute the callback in case of success", function () {
-      appMock.app.start.returns(appMock.initDeferred);
-      var cb = sinon.spy();
-      appMock.hullInit({}, cb);
-      appMock.initDeferred.resolve();
-      cb.should.have.been.called;
-      cb.should.have.been.calledWith(window.Hull);
+    describe("Evaluating the module", function () {
+      beforeEach(appMock.resetApp);
+
+      it("should return an object with 'app' and 'config key'", function () {
+        var conf = {};
+        appMock.hullInit(conf).should.contain.keys('config');
+        appMock.hullInit(conf).should.contain.keys('app');
+      });
+      it("should have an Aura application as the value of the 'app' config", function () {
+        var conf = {};
+        appMock.hullInit(conf).app.should.eql(appMock.app);
+      });
+      it("should return the config passed to the function into the 'config' property", function () {
+        var conf = {
+          foo: "FOO",
+          bar: "BAR",
+          baz: "BAZ"
+        };
+        appMock.hullInit(conf).config.should.contain.keys(Object.keys(conf));
+      });
+      it("should add a 'namespace property in the onfig and set it to 'Hull'", function () {
+        var conf = {};
+        appMock.hullInit(conf).config.should.contain.keys('namespace');
+        appMock.hullInit(conf).config.namespace.should.eql('hull');
+      });
     });
+
+    describe("should give feedback", function () {
+      beforeEach(appMock.resetApp);
+
+      it("should throw an exception if the init fails and no errback is provided", function (done) {
+        appMock.initDeferred.always(function () { done(); });
+        try {
+          appMock.hullInit({});
+        } catch (e) {
+          done();
+        }
+        appMock.initDeferred.reject();
+      });
+
+      it("should execute the errback in case of error", function () {
+        appMock.app.start.returns(appMock.initDeferred);
+        var errb = sinon.spy();
+        appMock.hullInit({}, null, errb);
+        appMock.initDeferred.reject();
+        errb.should.have.been.called;
+      });
+
+      it("should execute the callback in case of success", function () {
+        appMock.app.start.returns(appMock.initDeferred);
+        var cb = sinon.spy();
+        appMock.hullInit({}, cb);
+        appMock.initDeferred.resolve();
+        cb.should.have.been.called;
+        cb.should.have.been.calledWith(window.Hull);
+      });
+    });
+
   });
 });
--- spec/lib/hullbase_spec.js
+++ spec/lib/hullbase_spec.js
@@ -1,4 +1,4 @@
-/*global define:true, describe:true, it:true, sinon:true, before:true, after:true */
+/*global beforeEach:true, define:true, describe:true, it:true, sinon:true, before:true*/
 define(function () {
   "use strict";
 
@@ -12,107 +12,109 @@ define(function () {
     }
   };
 
-  var hullbase;
-  before(function (done){
-    require(['lib/hullbase'], function (base) {
-      hullbase = base;
-      done();
-    });
-  });
-
-  describe("Hull base module", function () {
-    it('should be exposed as window.Hull', function () {
-      hullbase.should.equal(window.Hull);
-    });
-  });
-
-  describe("Creating widget", function () {
-    describe("the widget name", function () {
-      it("should be truthy", function () {
-        ['', false, null, undefined].forEach(function (v) {
-          hullbase.widget.bind(undefined, v).should.throw('A widget must have a identifier');
-        });
-      });
-
-      it("should be a String", function () {
-        [true, [], new Date(), {}].forEach(function (v) {
-          hullbase.widget.bind(undefined, v).should.throw('The widget identifier must be a String');
-        });
-
-        hullbase.widget.bind(undefined, "widget_name", {}).should.not.throw('A widget must have a identifier');
-        hullbase.widget.bind(undefined, String("widget_name"), {}).should.not.throw('A widget must have a identifier');
-      });
-    });
-
-    describe("the widget definition", function () {
-      it("should be an Object literal or a function returning an Object literal", function () {
-        hullbase.widget.bind(undefined, "widget_name").should.throw('The widget widget_name must have a definition');
-        hullbase.widget.bind(undefined, "widget_name", function () {}).should.throw('The widget widget_name must have a definition');
-
-        hullbase.widget.bind(undefined, "widget_name", {}).should.not.throw('The widget widget_name must have a definition');
-        hullbase.widget.bind(undefined, "widget_name", function () { return {}; }).should.not.throw('The widget widget_name must have a definition');
-      });
-
-      it("should maintain the widget type or set 'Hull' as a default", function () {
-        hullbase.widget("w1", {}).type.should.eq("Hull");
-        hullbase.widget("w1", {type: "myType"}).type.should.eq("myType");
-      });
-    });
-
-    it("should return the widget definition", function () {
-      var widgetDef = {v: "value"};
-      var definedWidget = hullbase.widget("w1", widgetDef);
-      definedWidget.should.contain.keys(Object.keys(widgetDef));
-      definedWidget.should.contain.key('type');
-      definedWidget.type.should.equal('Hull');
-
-      var widgetDef = {type: "myType", v: "value"};
-      hullbase.widget("w1", widgetDef).should.equal(widgetDef);
-    });
-
-    describe('the associated module', function () {
-      it("should be defined in the default widget source", function (done) {
-        var definedModule = hullbase.widget("w2", {});
-        require(['__widget__$w2@default'], function (mod) {
-          mod.should.equal(definedModule);
-          done();
-        });
-      });
-
-      it("should be defined with the widgetDefinition as its value", function (done) {
-        var definedModule = hullbase.widget("w3", {});
-        require(['__widget__$w3@default'], function (mod) {
-          mod.should.equal(definedModule);
-          done();
-        });
-      });
-    });
-  });
-
-  // When the errback is not provided, an exception is thrown, supposed to be uncaught.
-  // But as we are backed by requireJS, it catches it and redisplays it nicely
-  // That's not a bad situation after all
-  describe("Initializing the application", function () {
-
-    var spy = sinon.spy();
-    var libHullMock = function (conf, cb, errb) {
-      spy();
-      cb();
-    };
-
-    // Mocking dependencies of  lib/hullbase
-    beforeEach(function (done) {
-      require.undef('lib/hull');
-      define('lib/hull', function () { return libHullMock;});
-      require(['lib/hull'], function () {
+  describe('Hull global wrapper', function () {
+    var hullbase;
+    before(function (done){
+      require(['lib/hullbase'], function (base) {
+        hullbase = base;
         done();
       });
     });
 
-    it("should call the module lib/hull once it is available", function (done) {
-      hullbase.init({}, function () {
-        spy.should.have.been.called;
-        done();
+    describe("Hull base module", function () {
+      it('should be exposed as window.Hull', function () {
+        hullbase.should.equal(window.Hull);
+      });
+    });
+
+    describe("Creating widget", function () {
+      describe("the widget name", function () {
+        it("should be truthy", function () {
+          ['', false, null, undefined].forEach(function (v) {
+            hullbase.widget.bind(undefined, v).should.throw('A widget must have a identifier');
+          });
+        });
+
+        it("should be a String", function () {
+          [true, [], new Date(), {}].forEach(function (v) {
+            hullbase.widget.bind(undefined, v).should.throw('The widget identifier must be a String');
+          });
+
+          hullbase.widget.bind(undefined, "widget_name", {}).should.not.throw('A widget must have a identifier');
+          hullbase.widget.bind(undefined, String("widget_name"), {}).should.not.throw('A widget must have a identifier');
+        });
+      });
+
+      describe("the widget definition", function () {
+        it("should be an Object literal or a function returning an Object literal", function () {
+          hullbase.widget.bind(undefined, "widget_name").should.throw('The widget widget_name must have a definition');
+          hullbase.widget.bind(undefined, "widget_name", function () {}).should.throw('The widget widget_name must have a definition');
+
+          hullbase.widget.bind(undefined, "widget_name", {}).should.not.throw('The widget widget_name must have a definition');
+          hullbase.widget.bind(undefined, "widget_name", function () { return {}; }).should.not.throw('The widget widget_name must have a definition');
+        });
+
+        it("should maintain the widget type or set 'Hull' as a default", function () {
+          hullbase.widget("w1", {}).type.should.eq("Hull");
+          hullbase.widget("w1", {type: "myType"}).type.should.eq("myType");
+        });
+      });
+
+      it("should return the widget definition", function () {
+        var widgetDef = {v: "value"};
+        var definedWidget = hullbase.widget("w1", widgetDef);
+        definedWidget.should.contain.keys(Object.keys(widgetDef));
+        definedWidget.should.contain.key('type');
+        definedWidget.type.should.equal('Hull');
+
+        widgetDef = {type: "myType", v: "value"};
+        hullbase.widget("w1", widgetDef).should.equal(widgetDef);
+      });
+
+      describe('the associated module', function () {
+        it("should be defined in the default widget source", function (done) {
+          var definedModule = hullbase.widget("w2", {});
+          require(['__widget__$w2@default'], function (mod) {
+            mod.should.equal(definedModule);
+            done();
+          });
+        });
+
+        it("should be defined with the widgetDefinition as its value", function (done) {
+          var definedModule = hullbase.widget("w3", {});
+          require(['__widget__$w3@default'], function (mod) {
+            mod.should.equal(definedModule);
+            done();
+          });
+        });
+      });
+    });
+
+    // When the errback is not provided, an exception is thrown, supposed to be uncaught.
+    // But as we are backed by requireJS, it catches it and redisplays it nicely
+    // That's not a bad situation after all
+    describe("Initializing the application", function () {
+
+      var spy = sinon.spy();
+      var libHullMock = function (conf, cb, errb) {
+        spy();
+        cb();
+      };
+
+      // Mocking dependencies of  lib/hullbase
+      beforeEach(function (done) {
+        require.undef('lib/hull');
+        define('lib/hull', function () { return libHullMock;});
+        require(['lib/hull'], function () {
+          done();
+        });
+      });
+
+      it("should call the module lib/hull once it is available", function (done) {
+        hullbase.init({}, function () {
+          spy.should.have.been.called;
+          done();
+        });
       });
     });
   });
