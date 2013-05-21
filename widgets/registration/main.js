fca721eb67fc08b158798fc7f5c89123392ef0be
Romain Dardour <romain@hull.io>
2013-05-21 00:50:48 -0600

    [Registration] Use Data attributes to find error DOM elements

--- widgets/registration/main.js
+++ widgets/registration/main.js
@@ -52,7 +52,8 @@ define(['underscore', 'h5f'], function(_, H5F) {
       this.$el.find('[data-hull-input]').each(function(key, el){
         var $el = $(el),
             id = $el.attr('id');
-        $('#'+id+'-error').text((el.checkValidity()) ? '' : $el.data('errorMessage'));
+        var errorMsg = $el.siblings('[data-hull-error]')
+        errorMsg.text((el.checkValidity()) ? '' : $el.data('errorMessage'));
       });
       return false;
     },
@@ -70,7 +71,6 @@ define(['underscore', 'h5f'], function(_, H5F) {
 
     beforeRender: function(data) {
       data.formId = this.formId;
-
       var fields = _.map(data.fields, function(f) {
         f.value = this._findFieldValue(f.name);
         return f;
