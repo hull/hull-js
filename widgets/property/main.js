c154a6fcbbc49e5d7852abd6103c459701e95cc8
Xavier Cambar <xcambar@gmail.com>
2013-05-24 11:45:28 +0200

    Creates the `property` widget

---
+++ widgets/property/main.js
@@ -0,0 +1,55 @@
+/**
+ *
+ * Displays a single property of an object.
+ *
+ * ## Examples
+ *
+ *     <div
+ *          data-hull-widget="property@hull"
+ *          data-hull-id="me"
+ *          data-hull-property="name"
+ *          data-hull-default-text="Not found"
+ *     ></div>
+ *
+ * ## Options
+ *
+ * - `id`: Required. The id of the object for which we want to display a property.
+ * - `property`: Required. The name of the property to be displayed. Can be a path
+ *   if the property is nested (_eg_ `stats.likes`)
+ *
+ * ## Template
+ *
+ * - `property`: Displays the property with no markup. Markup must be in the parents widget
+ *   for easier customization
+ *
+ */
+define({
+  type: 'Hull',
+
+  templates: [
+    'property'
+  ],
+
+  datasources: {
+    object: ':id'
+  },
+
+  refreshEvents: ['model.hull.me.change'],
+
+  beforeRender: function(data) {
+    "use strict";
+    var defaultText = this.options.defaultText || 'aa';
+    var value = this.findProp(data.object, this.options.property);
+    return {
+      property: value !== undefined ? value : defaultText
+    };
+  },
+
+  findProp: function (obj, prop) {
+    "use strict";
+    prop.split('.').forEach(function (_p) {
+      obj = !obj ? undefined : obj[_p];
+    });
+    return obj;
+  }
+});
