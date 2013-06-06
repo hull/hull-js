316d27d28412c2a5a0fa196031b3b89b6fe03e26
Romain Dardour <romain@hull.io>
2013-06-06 08:43:51 -0600

    Refactor voting widget

--- widgets/vote/main.js
+++ widgets/vote/main.js
@@ -27,22 +27,23 @@ define({
   templates: ['vote'],
 
   datasources: {
-    vote: 'me/reviews/:id',
-    votes: ':id/reviews'
+    vote: function(){
+      if(this.loggedIn()){
+        return this.api(this.id+'/reviews/me');
+      } else {
+        return false;
+      }
+    },
+    target: function(){
+      return this.api(this.id);
+    }
   },
 
   beforeRender: function(data){
-    data.split = {yes:0, no:0, blank:0 };
-
-    _.map(data.votes,function(vote){
-      if(vote.rating>0){
-        data.split.yes++;
-      } else if (vote.rating<0){
-        data.split.no++
-      } else {
-        data.split.blank++;
-      }
-    });
+    data.split = {};
+    data.split.blank = data.target.stats.reviews.distribution['0'];
+    data.split.yes = data.target.stats.reviews.distribution['1'];
+    data.split.no = data.target.stats.reviews.distribution['-1'];
     return data;
   },
 
@@ -55,7 +56,7 @@ define({
         description: description
       };
       this.api(this.id + '/reviews', 'post', d).then(function() {
-        self.render(self.getTemplate(), {vote:d});
+        self.render();
       });
     }
     evt.stopPropagation();
