55acda53b9c2047635c7ee4de6ed9505c113934e
Xavier Cambar <xcambar@gmail.com>
2013-07-03 16:46:17 -0600

    Fixes scopes in admin/reset widget

--- widgets/admin/reset/main.js
+++ widgets/admin/reset/main.js
@@ -21,8 +21,9 @@ Hull.define({
 
   beforeRender: function(data){
     data.relations = this.options.relations.split(',');
-    _.map(data.entities,function(entity){
-      entity.relations=_.map(data.relations,function(relation){
+    var self = this;
+    this.sandbox.util._.map(data.entities, function(entity){
+      entity.relations=self.sandbox.util._.map(data.relations,function(relation){
         var rel = entity.stats[relation] || 0;
         if (rel.count) rel = rel.count;
         return {
