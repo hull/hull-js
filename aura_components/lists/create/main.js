4a9a143a4dc92de342aa8bf15c7b4d42b48d4f36
Xavier Cambar <xcambar@gmail.com>
2013-09-23 16:14:32 +0200

    Creation of a list

---
+++ aura_components/lists/create/main.js
@@ -0,0 +1,32 @@
+/**
+ * ##Lists/create
+ *
+ * Allows to create a list. The list will be owned by the current user.
+ *
+ * ### Templates
+ *
+ * * `form`: How the contents of the list should be displayed
+ *
+ * ### Events:
+ *
+ * `form submit`: When the user submits the form included in the template, a list is created with the properties defined in the forms
+ */
+
+Hull.define({
+  type: 'Hull',
+  templates: ['form'],
+  refreshEvents: ['model.hull.me.change'],
+  actions: {
+    createList: function (e) {
+      "use strict";
+      e.preventDefault();
+      var self = this, inputs = {};
+      this.sandbox.dom.find('input', this.$el).each(function(c, input) {
+        if (input.getAttribute('type') === 'text') {
+          inputs[input.getAttribute('name')] = input.value;
+        }
+      });
+      this.api('me/lists', 'post', inputs).then(function() { self.render(); });
+    }
+  }
+});
