782e15cf11b4ac9994f0ec8d079ec3cb4d050233
Romain Dardour <romain@hull.io>
2013-09-29 18:12:24 +0200

    Redoc comments

--- aura_components/comments/list/main.js
+++ aura_components/comments/list/main.js
@@ -2,13 +2,13 @@
  * Displays comments on an object, that can be an internal Hull object (when you specify data-hull-id) or an external UID, (with data-hull-uid)
  * If using data-hull-uid, any unique string you can generate can be used to attach comments
  * 
- * @name Comments list
+ * @name List
  * @param {String} id/uid Required The object you want to comment on.
  * @param {String} focus  Optional Auto-Focus on the input field. default: false.
  * @datasource {comments} Collection of all the comments made on the object.
  * @action {comment} Submits a new comment.
- * @example <div data-hull-component="comments@hull" data-hull-id="HULL_OBJECT_ID"></div> // Allow to list and add comments on an object of the current application.
- * @example <div data-hull-component="comments@hull" data-hull-uid="http://path.to/my/url"></div> // If you want to reference any other Entity (for example the url of the current page)
+ * @example <div data-hull-component="comments@hull" data-hull-id="HULL_OBJECT_ID"></div>
+ * @example <div data-hull-component="comments@hull" data-hull-uid="http://path.to/my/url"></div>
  */
 
 Hull.define({
