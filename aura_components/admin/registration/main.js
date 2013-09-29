91ec60787652aa1ae365dee5ed1d806e4adf7818
Romain Dardour <romain@hull.io>
2013-09-29 17:36:21 +0200

    Refactor / redoc registration admin

--- aura_components/admin/registration/main.js
+++ aura_components/admin/registration/main.js
@@ -1,14 +1,18 @@
 /**
+ * 
+ * List registration form entries.
  *
- * ## Registration form admin panel
- *
+ * @name Admin: Registration form
+ * @template {admin} The main template. It shows the list of your quizzes or the form to edit a quiz.
+ * @template {list}  Show the list of your quizzes and a form to add new quizzes.
+ * @template {form}  Show the form to edit a quiz.
+ * @datasource {quizzes} The collection of all the quizzes available in the application.
+ * @example <div data-hull-component="admin/quiz@hull"></div>>
  */
 Hull.define({
   type: 'Hull',
 
-  templates: [
-    'intro'
-  ],
+  templates: ['registration'],
 
   datasources: {
     fields: function() {
@@ -18,29 +22,27 @@ Hull.define({
   },
 
   afterRender: function(data) {
-    this.$appSecret = this.$('#hull-app-secret');
-    this.$fieldsJSON = this.$('#hull-fields-json');
-
+    this.$fieldsJSON = this.$('[data-hull-field="field"]');
     this.$fieldsJSON.val(JSON.stringify(data.fields, null, 2));
   },
 
   actions: {
-    updateFields: function(e) {
+    update: function(e) {
       e.preventDefault();
 
       var data = {
         extra: { profile_fields: this.parseJSON(this.$fieldsJSON.val()) }
       };
 
-      this.updateFields(data);
+      this.update(data);
     }
   },
 
-  updateFields: function(data) {
+  update: function(data) {
     this.api('app', 'put', data).done(function() {
-      alert('Your fields has been saved');
+      alert('Your fields have been saved');
     }).fail(function() {
-      alert('Sothing went wrong, check your app secret and verify if your JSON is well formated');
+      alert("Something went wrong, check that you're logged in correctly");
     });
   },
 
