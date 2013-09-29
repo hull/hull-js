f204813ce68262f85052647b91ba485bcde59e3d
Romain Dardour <romain@hull.io>
2013-09-29 11:22:33 +0200

    Redoc Likes

--- aura_components/likes/list/main.js
+++ aura_components/likes/list/main.js
@@ -1,19 +1,15 @@
 /**
- * ## Likes list
- *
  * Shows the lists of all users who `liked` an object. Likes are not connected to facebook or any other network
  * You can use this as "favorite", "starred", "want"... or of course "like".
  *
- * ### Examples
- *
- *     <div data-hull-component="likes/list@hull" data-hull-id="HULL_ID"></div>
- *     <div data-hull-component="likes/list@hull" data-hull-id="YOUR_UNIQUE_ID"></div>
- *     <div data-hull-component="likes/list@hull" data-hull-id="ANY_URL"></div>
- *
- * ### Template
- *
- * - `list`: Shows the faces of all users who liked the object
- *
+ * @name Likes List
+ * @param {String} id/uid   Required The id/uid of the object on which you wants likes listed. Default value will be 'me'
+ * @datasource {target}     The object.
+ * @datasource {likes}      The likers for the object
+ * @template {list} Shows the faces of all users who liked the object
+ * @example <div data-hull-component="likes/list@hull" data-hull-id="HULL_ID"></div>
+ * @example <div data-hull-component="likes/list@hull" data-hull-id="YOUR_UNIQUE_ID"></div>
+ * @example <div data-hull-component="likes/list@hull" data-hull-id="ANY_URL"></div>
  */
 
 Hull.define({
@@ -31,11 +27,5 @@ Hull.define({
   },
   initialize: function () {
     this.actions.do();
-  },
-
-  actions: {
-    do: function () {
-      console.log(this);
-    }
   }
 });
