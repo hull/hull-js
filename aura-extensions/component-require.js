749245aca257b314fc0ded2541d89ef5df8ff1de
Xavier Cambar <xcambar@gmail.com>
2013-10-03 17:08:14 +0200

    :palmtree: Use your own modules or global one, I don't care, I'll resolve them both, bitch :palmtree:

--- aura-extensions/component-require.js
+++ aura-extensions/component-require.js
@@ -11,14 +11,29 @@ define(['underscore', 'jquery'], function(_, $) {
         var paths = _.map(this.require, function(path) {
           return nameToComponentPath(path, this);
         }, this);
-        require(paths, function(deps) {
-          dfd.resolve(deps);
-        }, function (err) {
-          dfd.reject(err);
+        var config = require.s.contexts._.config;
+        var defined = require.s.contexts._.defined;
+        var localRequire = require.config({
+          context: '__context__' + this.ref,
+          baseUrl: config.pkgs[this.ref].location,
+          shim: config.shim,
+          paths: config.paths
         });
-        this.require = function (name) {
-          return require(nameToComponentPath(name, this));
-        };
+        var component = this;
+        var componentRequire = this.require;
+        localRequire(['require'], function (require) {
+          _.each(_.keys(defined), function (name) {
+            define(name, [], function () { return defined[name]; });
+          });
+          require(componentRequire, function(deps) {
+            component.require = function (name) {
+              return localRequire(name);
+            };
+            dfd.resolve(deps);
+          }, function (err) {
+            dfd.reject(err);
+          });
+        });
         return dfd.promise();
       }
     });
