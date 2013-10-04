9b7aaee49b736f50bd91c124514e0a5a66b74307
Romain Dardour <romain@hull.io>
2013-10-04 18:37:22 +0200

    Move sweepstake, quiz flag, property and footer components

--- aura_components/utils/footer/main.js
+++ aura_components/utils/footer/main.js
@@ -2,7 +2,7 @@
  * Shows a 'built with hull' element
  *
  * @name Footer
- * @example <div data-hull-component="footer@hull"></div>
+ * @example <div data-hull-component="utils/footer@hull"></div>
  */
 Hull.component({
 
