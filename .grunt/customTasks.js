6d8973b1263b99183fb50e7dc6db6012efa09433
Xavier Cambar <xcambar@gmail.com>
2013-10-09 14:05:07 +0200

    Fixes tag naming in deploy

--- .grunt/customTasks.js
+++ .grunt/customTasks.js
@@ -8,11 +8,18 @@ module.exports = function (grunt) {
   grunt.registerTask("version", "generate a file from a template", function () {
     var conf = grunt.config("version");
     var done = this.async();
-    git.branch(function(branch){
-      grunt.config.set("PKG_VERSION", branch);
+    function write (name) {
+      grunt.config.set("PKG_VERSION", name);
       grunt.file.write(conf.dest, grunt.template.process(conf.template));
       grunt.log.writeln('Generated version description for \'' + conf.dest + '\' successfully.');
       done();
+    }
+    git.branch(function(branch){
+      if (branch === 'HEAD') {
+        git.tag(write);
+      } else {
+        write(branch);
+      }
     });
 
   });
