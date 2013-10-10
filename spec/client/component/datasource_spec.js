0dd8b419bc2f19da11a44397c30d5be2b88be338
Xavier Cambar <xcambar@gmail.com>
2013-10-10 19:23:55 +0200

    Starts specs for the component/datasource extension

--- spec/client/component/datasource_spec.js
+++ spec/client/component/datasource_spec.js
@@ -1,24 +1,68 @@
 define(['lib/client/component/datasource'], function (module) {
+  before(function () {
+    this.module = module;
+    this.dsModelStub = sinon.stub(module, 'datasourceModel');
+    this.dsModelStub.returns(true);
+  });
+  afterEach(function () {
+    this.dsModelStub.reset();
+  });
+
   describe("Extending components with datasources", function () {
     beforeEach(function () {
-      sinon.stub(module, 'datasourceModel');
-      this.apiSpy = sinon.spy();
       this.beforeSpy = sinon.spy();
-      this.DatasourceSpy = sinon.spy();
       this.appMock = {
         components: { before:this.beforeSpy },
-        core: {data: {api: {model: this.beforeSpy}}}
+        core: {data: {api: {model: function () {}}}}
       };
       this.module = module;
     });
+
     it("should attach the datasource injection method before Component::initialize", function () {
       this.module.initialize(this.appMock);
-      this.beforeSpy.should.have.been.calledWith('initialize');
-      var arg2 = this.beforeSpy.args[0][1];
-      arg2.should.be.a('function');
+      this.beforeSpy.withArgs('initialize').should.have.been.calledOnce;
+      var wrapper = this.beforeSpy.withArgs('initialize').args[0][1];
+      wrapper.should.be.a('function');
       //Check that the function wraps the injection method
-      arg2({});
+      var spy = sinon.spy(this.module, 'addDatasources');
+      module.addDatasources.should.not.have.been.called;
+      wrapper({});
+      module.addDatasources.should.have.been.calledOnce;
+      spy.reset();
     });
 
+    it("should attach the datasource resolution method before Component::render", function () {
+      this.module.initialize(this.appMock);
+      this.beforeSpy.withArgs('render').should.have.been.calledOnce;
+      var arg2 = this.beforeSpy.withArgs('render').args[0][1];
+      arg2.should.be.equal(module.fetchDatasources);
+    });
+  });
+
+  describe("Default datasources", function () {
+    beforeEach(function () {
+      this.appMock = {
+        components: { before: function () {} },
+        core: {data: {api: {model: function () {}}}}
+      };
+      this.apiStub = sinon.stub(this.appMock.core.data.api, 'model', function (arg) {
+        return arg;
+      });
+    });
+    it('should fetch the default datasources', function () {
+      this.module.initialize(this.appMock);
+      this.apiStub.should.have.been.calledThrice;
+      this.apiStub.withArgs('me').should.have.been.calledOnce;
+      this.apiStub.withArgs('app').should.have.been.calledOnce;
+      this.apiStub.withArgs('org').should.have.been.calledOnce;
+    });
+
+    it('should instanciate datasourceModels with the default datasources', function () {
+      this.module.initialize(this.appMock);
+      this.dsModelStub.should.have.been.calledThrice;
+      this.dsModelStub.withArgs('me').should.have.been.calledOnce;
+      this.dsModelStub.withArgs('app').should.have.been.calledOnce;
+      this.dsModelStub.withArgs('org').should.have.been.calledOnce;
+    });
   });
 });
--- src/client/component/datasource.coffee
+++ src/client/component/datasource.coffee
@@ -2,31 +2,41 @@ define ['lib/client/datasource', 'underscore'], (Datasource, _)->
   onDataError = (datasourceName, err)->
     console.log "An error occurred with datasource #{datasourceName}", err
   module =
+    datasourceModel: Datasource
+    
+    # Adds datasources to the instance of the component
+    addDatasources: (datasources)->
+      _.each datasources, (ds, i)=>
+        ds = _.bind ds, @ if _.isFunction ds
+        @datasources[i] = new module.datasourceModel(ds, @api) unless ds instanceof module.datasourceModel
+    
+    # Fetches all the datasources for the instance of the component
+    fetchDatasources: ()->
+      @data ?= {}
+      keys = _.keys(@datasources)
+      promiseArray  = _.map keys, (k)=>
+        ds = @datasources[k]
+        ds.parse(_.extend({}, @, @options || {}))
+        handler = @["on#{_.string.capitalize(_.string.camelize(k))}Error"]
+        handler = onDataError unless _.isFunction(handler)
+        handler = _.bind(handler, @) if _.isFunction(handler)
+        ds.fetch().then (res)=>
+          @data[k] = if res.toJSON then res.toJSON() else res
+        , (err)->
+          handler(k, err)
+      @sandbox.data.when(promiseArray...)
+
+    # Registers hooks and creates default datasources
     initialize: (app)->
       default_datasources =
-        me: new Datasource app.core.data.api.model('me')
-        app: new Datasource app.core.data.api.model('app')
-        org: new Datasource app.core.data.api.model('org')
+        me: new module.datasourceModel app.core.data.api.model('me')
+        app: new module.datasourceModel app.core.data.api.model('app')
+        org: new module.datasourceModel app.core.data.api.model('org')
 
       app.components.before 'initialize', (options)->
-        @datasources = _.extend {}, default_datasources, @datasources, options.datasources
-        _.each @datasources, (ds, i)=>
-          ds = _.bind ds, @ if _.isFunction ds
-          @datasources[i] = new Datasource(ds, @api) unless ds instanceof Datasource
+        datasources = _.extend {}, default_datasources, @datasources, options.datasources
+        module.addDatasources.call(@, datasources)
 
-      app.components.before 'render', ()->
-        @data ?= {}
-        keys = _.keys(@datasources)
-        promiseArray  = _.map keys, (k)=>
-          ds = @datasources[k]
-          ds.parse(_.extend({}, @, @options || {}))
-          handler = @["on#{_.string.capitalize(_.string.camelize(k))}Error"]
-          handler = onDataError unless _.isFunction(handler)
-          handler = _.bind(handler, @) if _.isFunction(handler)
-          ds.fetch().then (res)=>
-            @data[k] = if res.toJSON then res.toJSON() else res
-          , (err)->
-            handler(k, err)
-        @sandbox.data.when(promiseArray...)
+      app.components.before 'render', module.fetchDatasources
 
   module
