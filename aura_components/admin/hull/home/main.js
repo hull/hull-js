2cb18e5ed74c24209d33a92b6db22b10a3f8acf4
Xavier Cambar <xcambar@gmail.com>
2014-02-21 16:13:20 +0100

    Adds password recovery + an info message for devs showing which version of hull.js is running

--- aura_components/admin/hull/home/main.js
+++ aura_components/admin/hull/home/main.js
@@ -1,38 +1,37 @@
 Hull.component({
   refreshEvents: ['model.hull.me.change'],
-  templates: ['main'],
+  templates: ['main', 'recover'],
 
   initialize: function () {
     this.errors = [];
-    this.sandbox.on('hull.auth.login', function (me) {
-      if (me.approved) {
-        window.location.pathname = '/';
-      }
-    });
+    this.messages = [];
+    this.sandbox.on('hull.auth.login', this.redirectIfApproved);
+  },
+  redirectIfApproved: function (me) {
+    if (me.approved) {
+      window.location.pathname = '/';
+    }
   },
   actions: {
     showLogin: function () {
-      this.clearErrors();
+      this.clearMessages();
       this.wantsSignup = false;
       this.refresh();
     },
     showSignup: function () {
-      this.clearErrors();
+      this.clearMessages();
       this.wantsSignup = true;
       this.refresh();
     },
     doLogin: function (evt, ctx) {
       var self = this;
-      this.clearErrors();
+      this.clearMessages();
       var formData = this.getFormData();
-      this.sandbox.login(formData.email, formData.password).then(function (me) {
-        if (me.approved) {
-          window.location.pathname = '/';
-        } else {
-          self.refresh();
-        }
-      }, function (err) {
-        self.errors.push(err.message);
+      this.sandbox.login(formData.email, formData.password).then(
+        this.redirectIfApproved,
+        function (err) {
+          self.errors.push(err.message);
+      }).then(function () {
         self.refresh();
       });
     },
@@ -44,23 +43,41 @@ Hull.component({
     },
     doSignup: function (evt, ctx) {
       var self = this;
-      this.clearErrors();
+      this.clearMessages();
       var formData = this.getFormData();
-      Hull.signup(formData).then(function (me) {
-        window.location.pathname = '/accounts/bootstrap';
-      }, function (err) {
-        self.errors.push(err.message);
-        self.refresh();
+      Hull.signup(formData).then(
+        this.actions.doBootstrap,
+        function (err) {
+          self.errors.push(err.message);
+          self.refresh();
       });
     },
     doBootstrap: function () {
       window.location.pathname = '/accounts/bootstrap';
+    },
+    toggleTemplate: function (el, ctx) {
+      this.clearMessages();
+      this.template = ctx.data.template || this.templates[0];
+      this.refresh();
+    },
+    doRecoverPassword: function () {
+      this.clearMessages();
+      var self = this;
+      var email = this.getFormData().email;
+      this.api.post('users/request_password_reset', {email: email}).then(function () {
+        self.messages.push('An email has been sent. Check your inbox.');
+      }, function () {
+        self.errors.push('This is not a valid email address');
+      }).then(function () {
+        self.refresh();
+      });
     }
   },
 
   beforeRender: function (data) {
     data.wantsSignup = this.wantsSignup;
     data.errors = this.errors;
+    data.messages = this.messages;
   },
 
   getFormData: function () {
@@ -69,7 +86,8 @@ Hull.component({
       password: this.$find('#password').val()
     };
   },
-  clearErrors: function () {
+  clearMessages: function () {
+    this.messages.splice(0, this.messages.length);
     this.errors.splice(0, this.errors.length);
   }
 });
