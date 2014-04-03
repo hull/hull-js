d4baeaffb48914609929e51fc12ce361d61f804e
Xavier Cambar <xcambar@gmail.com>
2014-04-03 17:31:21 +0200

    Adds search capability, adds close button to modal

--- aura_components/admin/users_list/main.js
+++ aura_components/admin/users_list/main.js
@@ -18,7 +18,7 @@ Hull.component({
 
   refreshEvents: ['model.hull.me.change'],
 
-  require: ['spin.min', 'ladda.min', 'bootstrap-modal', 'bootstrap-modalmanager', 'datagrid'],
+  require: ['spin.min', 'ladda.min', 'bootstrap-modal', 'bootstrap-modalmanager', 'search', 'datagrid'],
 
   renderError: function(err) {
     if (err.message.status === 401) {
@@ -61,100 +61,14 @@ Hull.component({
 
   beforeRender: function(data){
     var datasource = this.datasources.users;
-
-    data.showPagination = datasource.isPaginable();
-    data.showNextButton = !datasource.isLast();
-    data.showPreviousButton = !datasource.isFirst();
-
-    data.currentQuery = this.currentQuery;
-
-    data.filters = {
-      All: { action: 'resetFilter', isActive: this.query.approved == null },
-      Approved: { action: 'filterApproved', isActive: this.query.approved === true },
-      Unapproved: { action: 'filterUnapproved', isActive: this.query.approved === false }
-    };
   },
 
   afterRender: function() {
-    var $searchForm = this.$('.js-hull-users-search');
-    $searchForm.on('submit', this.sandbox.util._.bind(function(e) {
-      e.preventDefault();
-      this.search($searchForm.find('.js-hull-users-search-query').val());
-    }, this));
-    var dom = this.sandbox.dom.find;
-    this.sandbox.util._.each(this.$('.ladda-button'), function (btn) {
-      var lbtn = Ladda.create(btn);
-      dom(btn).data('ladda', lbtn);
-    });
-
-
     var self = this;
     var datasource = {
-      columns: function () {
-        return [{
-          property: 'picture',
-          label: '',
-          sortable: false,
-          render: function (value, row) {
-            return $('<div>')
-              .attr('data-hull-component', 'admin/user_avatar@hull')
-              .attr('data-hull-avatar', value);
-          }
-        }, {
-          property: 'name',
-          label: 'Name',
-          sortable: true,
-          render: function (name, row) {
-            if (!name && row.main_identity.toLowerCase() === 'guest') {
-              name = 'Guest user';
-            }
-            var email = row.email || ''
-            var $name = $('<p class="name">').text(name);
-            var $email = $('<span class="email">');
-            var $emailAnchor = $('<a>').attr('href', 'mailto:' + email).text(email);
-            if (name) {
-              if (!email) $email.hide();
-              return [$name.text(name), $email.append($emailAnchor)];
-            } else {
-              return $name.html($emailAnchor);
-            }
-          }
-        }, {
-          property: 'created_at',
-          label: 'Signed up since',
-          sortable: true,
-          render: function (value, row) {
-            return self.sandbox.util.moment(value).fromNow();
-          }
-        }, {
-          property: 'stats',
-          label: 'Visits',
-          sortable: false,
-          render: function (stats, row) {
-            var signInCount = 1 + parseInt(stats.sign_in_count || 0);
-            var $total = $('<p>')
-              .text('Total: ')
-              .append($('<strong>').text(signInCount));
-            var $latest = $('<small>').text('Latest: ').append($('<strong>').text(self.sandbox.util.moment(row.last_seen_at || row.created_at).fromNow()));
-
-            return [$total, $latest];
-          }
-        }, {
-          property: 'main_identity',
-          label: 'Identities',
-          sortable: false,
-          render: function (value, row) {
-            var providers = self.getLoginProviders(row);
-            return self.sandbox.util._.map(providers, function (provider) {
-              return $('<i>').addClass('icon-' + provider);
-            });
-          }
-        }];
-      },
+      columns: this.sandbox.util._.bind(this.columnConfig, this),
       data: function (options, callback) {
         var ds = self.datasources.users;
-        ds.def.params.page = (options.pageIndex || 0) + 1;
-        ds.def.params.per_page = options.pageSize || 30;
         var trackingData = {
           page: ds.def.params.page,
           user_id: self.data.me.id
@@ -163,14 +77,27 @@ Hull.component({
           var direction = options.sortDirection || '';
           trackingData.sortProperty = options.sortProperty;
           trackingData.sortDirection = direction;
-        }
-        self.sandbox.track('hull.admin.users_list.pageview', trackingData);
-        if (options.sortProperty) {
           var sortChanged = ds.sort(trackingData.sortProperty, trackingData.sortDirection);
           if (sortChanged) {
             self.sandbox.track('hull.admin.users_list.sort', trackingData);
           }
         }
+        if (options.search) {
+          self.sandbox.track('hull.admin.users_list.search', {
+            user_id: self.data.me.id,
+            search: options.search
+          });
+
+          var whereParams = {};
+          whereParams.email = options.search;
+          ds.where(whereParams);
+          trackingData.search = options.search;
+        } else {
+          ds.where({});
+        }
+        ds.def.params.page = (options.pageIndex || 0) + 1;
+        ds.def.params.per_page = options.pageSize || 30;
+        self.sandbox.track('hull.admin.users_list.pageview', trackingData);
         ds.fetch().then(function (obj) {
           var payload = {
             data: obj.toJSON(),
@@ -199,9 +126,69 @@ Hull.component({
       self.sandbox.emit('hull.user.select', row.id);
       $table.datagrid('clearSelectedItems');
     });
+  },
 
+  columnConfig: function () {
+    var self = this;
+    return [{
+      property: 'picture',
+      label: '',
+      sortable: false,
+      render: function (value, row) {
+        return $('<div>')
+          .attr('data-hull-component', 'admin/user_avatar@hull')
+          .attr('data-hull-avatar', value);
+      }
+    }, {
+      property: 'name',
+      label: 'Name',
+      sortable: true,
+      render: function (name, row) {
+        if (!name && row.main_identity.toLowerCase() === 'guest') {
+          name = 'Guest user';
+        }
+        var email = row.email || ''
+        var $name = $('<p class="name">').text(name);
+        var $email = $('<span class="email">');
+        var $emailAnchor = $('<a>').attr('href', 'mailto:' + email).text(email);
+        if (name) {
+          if (!email) $email.hide();
+          return [$name.text(name), $email.append($emailAnchor)];
+        } else {
+          return $name.html($emailAnchor);
+        }
+      }
+    }, {
+      property: 'created_at',
+      label: 'Signed up since',
+      sortable: true,
+      render: function (value, row) {
+        return self.sandbox.util.moment(value).fromNow();
+      }
+    }, {
+      property: 'stats',
+      label: 'Visits',
+      sortable: false,
+      render: function (stats, row) {
+        var signInCount = 1 + parseInt(stats.sign_in_count || 0);
+        var $total = $('<p>')
+          .text('Total: ')
+          .append($('<strong>').text(signInCount));
+        var $latest = $('<small>').text('Latest: ').append($('<strong>').text(self.sandbox.util.moment(row.last_seen_at || row.created_at).fromNow()));
 
-
+        return [$total, $latest];
+      }
+    }, {
+      property: 'main_identity',
+      label: 'Identities',
+      sortable: false,
+      render: function (value, row) {
+        var providers = self.getLoginProviders(row);
+        return self.sandbox.util._.map(providers, function (provider) {
+          return $('<i>').addClass('icon-' + provider);
+        });
+      }
+    }];
   },
 
   getLoginProviders: function (user) {
@@ -218,64 +205,8 @@ Hull.component({
   },
 
   actions: {
-    nextPage: function() {
-      var datasource = this.datasources.users;
-      if (!datasource.isLast()) {
-        this.$('[data-hull-action=nextPage].ladda-button').data('ladda').start();
-        datasource.next();
-        this.render();
-      }
-    },
-
-    previousPage: function() {
-      var datasource = this.datasources.users;
-      if (!datasource.isFirst()) {
-        this.$('[data-hull-action=previousPage].ladda-button').data('ladda').start();
-        datasource.previous();
-        this.render();
-      }
-    },
-
     selectUser: function(event, action) {
       this.sandbox.emit('hull.user.select', action.data.id);
-    },
-
-    sort: function(event, action) {
-      this.sort(action.data.field, action.data.direction);
-    },
-
-    resetSearch: function() {
-      delete this.query.email;
-      this.filter();
-    },
-
-    resetFilter: function() {
-      delete this.query.approved;
-      this.filter();
-    },
-
-    filterApproved: function() {
-      this.query.approved = true;
-      this.filter();
-    },
-
-    filterUnapproved: function() {
-      this.query.approved = false;
-      this.filter();
-    }
-  },
-
-  sort: function(field, direction) {
-    this.datasources.users.sort(field, direction);
-    this.render();
-  },
-
-  filter: function() {
-    if(this.queryHasChanged()) {
-      this.datasources.users.where(this.query);
-      this.render();
-
-      this.currentQuery = this.sandbox.util._.clone(this.query);
     }
   },
 
@@ -289,15 +220,4 @@ Hull.component({
       return this.currentQuery[k] === v;
     }, this);
   },
-
-  search: function(email) {
-    var query;
-    if (!this.sandbox.util._.string.isBlank(email)) {
-      this.query.email = email;
-    } else {
-      delete this.query.email;
-    }
-
-    this.filter();
-  }
 });
