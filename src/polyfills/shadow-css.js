9e5c77626811d86e5fce1cd5c69503d10b58ae4c
Romain Dardour <romain@hull.io>
2015-04-28 22:39:47 +0200

    Modify HTMLImports behaviour so we can scope styles

--- src/polyfills/shadow-css.js
+++ src/polyfills/shadow-css.js
@@ -1,64 +1,56 @@
-var SHIM_ATTRIBUTE = 'shim-shadowdom';
+var SHIM_ATTRIBUTE = 'rel';
 var SHIMMED_ATTRIBUTE = 'shim-shadowdom-css';
 var NO_SHIM_ATTRIBUTE = 'no-shim';
 
-// document.addEventListener('DOMContentLoaded', function() {
-//   if (window.HTMLImports && !HTMLImports.useNative) {
-//     var SHIM_SHEET_SELECTOR = 'link[rel=stylesheet]';
-//     HTMLImports.importer.documentPreloadSelectors += ',' + SHIM_SHEET_SELECTOR;
-//     HTMLImports.importer.importsPreloadSelectors += ',' + SHIM_SHEET_SELECTOR;
+module.exports = function(){
+  if (window.HTMLImports && !HTMLImports.useNative) {
+    var SHIM_SHEET_SELECTOR = 'link[rel=stylesheet]';
+    HTMLImports.importer.documentPreloadSelectors += ',' + SHIM_SHEET_SELECTOR;
+    HTMLImports.importer.importsPreloadSelectors += ',' + SHIM_SHEET_SELECTOR;
 
-//     HTMLImports.parser.documentSelectors = [
-//       HTMLImports.parser.documentSelectors,
-//       SHIM_SHEET_SELECTOR,
-//     ].join(',');
+    HTMLImports.parser.documentSelectors = [
+      HTMLImports.parser.documentSelectors,
+      SHIM_SHEET_SELECTOR,
+    ].join(',');
 
-//     var originalParseGeneric = HTMLImports.parser.parseGeneric;
+    var originalParseGeneric = HTMLImports.parser.parseGeneric;
 
-//     HTMLImports.parser.parseGeneric = function(elt) {
-//       if (elt[SHIMMED_ATTRIBUTE]) {
-//         return;
-//       }
-//       var style = elt.__importElement || elt;
-//       if (!style.hasAttribute(SHIM_ATTRIBUTE)) {
-//         originalParseGeneric.call(this, elt);
-//         return;
-//       }
-//       // if (elt.__resource) {
-//       //   style = elt.ownerDocument.createElement('style');
-//       //   style.textContent = elt.__resource;
-//       // }
-//       // relay on HTMLImports for path fixup
-//       // HTMLImports.path.resolveUrlsInStyle(style, elt.href);
-//       style.textContent = ShadowCSS.shimStyle(style);
-//       style.removeAttribute(SHIM_ATTRIBUTE, '');
-//       style.setAttribute(SHIMMED_ATTRIBUTE, '');
-//       style[SHIMMED_ATTRIBUTE] = true;
+    HTMLImports.parser.parseGeneric = function(elt) {
+      if (elt[SHIMMED_ATTRIBUTE]) { return; }
 
-//       // place in document
-//       if (style.parentNode !== head) {
-//         // replace links in head
-//         if (elt.parentNode === head) {
-//           head.replaceChild(style, elt);
-//         } else {
-//           this.addElementToDocument(style);
-//         }
-//       }
+      var style = elt.__importElement || elt;
 
-//       style.__importParsed = true;
-//       this.markParsingComplete(elt);
-//       this.parseNext();
+      if(!(style.nodeName=='STYLE' || style.nodeName=='LINK')){
+        originalParseGeneric.call(this, elt);
+        return;
+      }
 
-//     }
+      if (elt.__resource) {
+        style = elt.ownerDocument.createElement('style');
+        style.textContent = elt.__resource;
+      }
 
-//     var hasResource = HTMLImports.parser.hasResource;
-//     HTMLImports.parser.hasResource = function(node) {
-//       if (node.localName === 'link' && node.rel === 'stylesheet' && node.hasAttribute(SHIM_ATTRIBUTE)) {
-//         return (node.__resource);
-//       } else {
-//         return hasResource.call(this, node);
-//       }
-//     }
+      // relay on HTMLImports for path fixup
+      HTMLImports.path.resolveUrlsInStyle(style, elt.href);
 
-//   }
-// });
+      style.setAttribute(SHIMMED_ATTRIBUTE, '');
+      style[SHIMMED_ATTRIBUTE] = true;
+
+      elt.ownerDocument.body.appendChild(style)
+
+      this.markParsingComplete(elt);
+      this.parseNext();
+    }
+
+    var hasResource = HTMLImports.parser.hasResource;
+    HTMLImports.parser.hasResource = function(node) {
+      if (node.localName === 'link' && node.rel === 'stylesheet' && node.hasAttribute(SHIM_ATTRIBUTE)) {
+        return (node.__resource);
+      } else {
+        return hasResource.call(this, node);
+      }
+    }
+
+  }
+  true
+}
