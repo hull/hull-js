1aca28366e905973c1aeb4a2123e1589be488734
Romain Dardour <romain@hull.io>
2015-05-13 10:19:05 +0200

    fix shadow css polyfill

--- src/polyfills/shadow-css.js
+++ src/polyfills/shadow-css.js
@@ -4,6 +4,7 @@ var NO_SHIM_ATTRIBUTE = 'no-shim';
 
 module.exports = function(){
   if (window.HTMLImports && !HTMLImports.useNative) {
+
     var SHIM_SHEET_SELECTOR = 'link[rel=stylesheet]';
     HTMLImports.importer.documentPreloadSelectors += ',' + SHIM_SHEET_SELECTOR;
     HTMLImports.importer.importsPreloadSelectors += ',' + SHIM_SHEET_SELECTOR;
@@ -18,26 +19,31 @@ module.exports = function(){
     HTMLImports.parser.parseGeneric = function(elt) {
       if (elt[SHIMMED_ATTRIBUTE]) { return; }
 
-      // var style = elt.__importElement || elt;
-      // if(!(style.nodeName=='STYLE' || style.nodeName=='LINK')){
-      //   originalParseGeneric.call(this, elt);
-      //   return;
-      // }
+      var style = elt.__importElement || elt;
+
+      if(!(style.nodeName=='STYLE' || style.nodeName=='LINK')){
+        originalParseGeneric.call(this, elt);
+        return;
+      }
 
       // if (elt.__resource) {
       //   style = elt.ownerDocument.createElement('style');
       //   style.textContent = elt.__resource;
       // }
 
-      // // relay on HTMLImports for path fixup
+      // relay on HTMLImports for path fixup
       // HTMLImports.path.resolveUrlsInStyle(style, elt.href);
 
       // style.setAttribute(SHIMMED_ATTRIBUTE, '');
       // style[SHIMMED_ATTRIBUTE] = true;
 
+      // elt.parentNode.replaceChild(style, elt)
       // elt.ownerDocument.body.appendChild(style)
 
-      this.markParsingComplete(elt);
+      // this.trackElement(elt);
+
+      style.__importParsed = true;
+      this.markParsingComplete(style);
       this.parseNext();
     }
 
