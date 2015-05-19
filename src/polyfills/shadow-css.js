f2cab2fc375e0b849d02ef892ad2c5bc49564252
Romain Dardour <romain@hull.io>
2015-05-19 13:17:18 +0200

    Don't apply polyfill to root document.

--- src/polyfills/shadow-css.js
+++ src/polyfills/shadow-css.js
@@ -4,52 +4,32 @@ var NO_SHIM_ATTRIBUTE = 'no-shim';
 
 module.exports = function(){
   if (window.HTMLImports && !HTMLImports.useNative) {
+    var LINK_STYLE_SELECTOR = 'link[rel=stylesheet]';
 
-    var SHIM_SHEET_SELECTOR = 'link[rel=stylesheet]';
-    HTMLImports.importer.documentPreloadSelectors += ',' + SHIM_SHEET_SELECTOR;
-    HTMLImports.importer.importsPreloadSelectors += ',' + SHIM_SHEET_SELECTOR;
-
-    HTMLImports.parser.documentSelectors = [
-      HTMLImports.parser.documentSelectors,
-      SHIM_SHEET_SELECTOR,
-    ].join(',');
+    HTMLImports.importer.importsPreloadSelectors += ',' + LINK_STYLE_SELECTOR;
 
     var originalParseGeneric = HTMLImports.parser.parseGeneric;
-
     HTMLImports.parser.parseGeneric = function(elt) {
+
       if (elt[SHIMMED_ATTRIBUTE]) { return; }
 
       var style = elt.__importElement || elt;
 
-      if(!(style.nodeName=='STYLE' || style.nodeName=='LINK')){
+      if(
+          (style.nodeName=='STYLE' || style.nodeName=='LINK') &&
+          (style.ownerDocument !== document || this.rootImportForElement(style).getAttribute('rel')==='import')
+        ){
+        style.__importParsed = true;
+        this.markParsingComplete(style);
+        this.parseNext();
+      } else {
         originalParseGeneric.call(this, elt);
-        return;
       }
-
-      // if (elt.__resource) {
-      //   style = elt.ownerDocument.createElement('style');
-      //   style.textContent = elt.__resource;
-      // }
-
-      // relay on HTMLImports for path fixup
-      // HTMLImports.path.resolveUrlsInStyle(style, elt.href);
-
-      // style.setAttribute(SHIMMED_ATTRIBUTE, '');
-      // style[SHIMMED_ATTRIBUTE] = true;
-
-      // elt.parentNode.replaceChild(style, elt)
-      // elt.ownerDocument.body.appendChild(style)
-
-      // this.trackElement(elt);
-
-      style.__importParsed = true;
-      this.markParsingComplete(style);
-      this.parseNext();
     }
 
     var hasResource = HTMLImports.parser.hasResource;
     HTMLImports.parser.hasResource = function(node) {
-      if (node.localName === 'link' && node.rel === 'stylesheet' && node.hasAttribute(SHIM_ATTRIBUTE)) {
+      if (node.localName === 'link' && node.hasAttribute(SHIM_ATTRIBUTE) && node.rel === 'stylesheet' ) {
         return (node.__resource);
       } else {
         return hasResource.call(this, node);
