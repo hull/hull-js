f11217aedd613816153a87751b4bf55728db03ad
Jimmy Oliger <hi@jimmyoliger.com>
2015-08-11 21:26:27 +0200

    (re)log the customer in if multipass is enabled

--- src/client/initialize-platform.js
+++ src/client/initialize-platform.js
@@ -5,8 +5,17 @@ import emptyFunction from '../utils/empty-function';
 function initializeShopifyPlatform(context, currentConfig, hull) {
   const { customerId, accessToken } = currentConfig.get();
 
-  if (!customerId) {
-    hull.logout();
+  if (!customerId && hull.currentUser()) {
+      hull.api('services/shopify/login').then(function(r) {
+        // If the platform has multipass enabled we can log the customer in
+        // without knowing his password.
+        if (r.auth === 'multipass') {
+          let l = 'https://' + document.location.host + '/account/login/multipass/' + r.token;
+          window.location = l;
+        } else {
+          hull.logout();
+        }
+      });
   } else if (/^[0-9]+$/.test(customerId) && !accessToken) {
     hull.api('services/shopify/customers/' + customerId, 'put').then(function() {
       document.location.reload();
